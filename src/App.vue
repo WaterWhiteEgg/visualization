f013f1c4585eb39fe26f305c22bb0df3ec4e4488
White Egg <2733259150@qq.com>
2024-03-12 23:14:58 +0800

    加个ui框架，搜索框

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const emits = defineEmits<{
 }>();
 
 // 正则
-const chineseReg = /[^\u4e00-\u9fa5]/g;
+const chineseReg = /[^0-9\u4e00-\u9fa5]/g;
 // 尝试响应后端
 onMounted(() => {
   getWeather("120000").then((res) => {
@@ -19,46 +19,112 @@ onMounted(() => {
     console.log(res);
   });
 });
+// 加载getCitys的flag
+const isGetCitysFinally = ref(false)
+
+
 // 搜索城市编号
 const cityText = ref("");
 watch(cityText, (newVal, oldVal) => {
-  const change = debounce(() => {
+  const fn = debounce(() => {
     // 判断是否是中文
     if (!chineseReg.test(newVal)) {
-      getCitys(newVal).then((res) => {        
-        cityArray.value = res.data.data?.districts;
-        console.log(cityArray.value);
+      // 加载中flag
+      isGetCitysFinally.value = false
+      getCitys(newVal).then((res) => {
 
-      });
+        // 获得内容数组
+        let allArray = res.data.data?.districts as City[]
+        // 将所有的districts获取
+        const forDistricts = (inAllArray: City[]) => {
+          // console.log(inAllArray);
+          inAllArray.forEach((item) => {
+            // console.log(item);
+            // 判断是否再次遍历
+            let isAgain = false
+            //  递归
+
+            if (item.districts.length !== 0) {
+              isAgain = true
+              allArray = allArray.concat(item.districts)
+            }
+            if (isAgain) {
+              forDistricts(item.districts)
+            }
+          })
+        }
+        forDistricts(allArray)
+        cityArray.value = allArray;
+
+      })
+        // 加载完成后提示
+        .finally(() => {
+          // 加载完flag
+          isGetCitysFinally.value = true
+        })
     }
-  }, 1000);
-  change().then((res) => {
-    // console.log(res);
-  });
+    // 不符合规则将提示
+    else {
+
+    }
+  }, 500);
+  fn()
 });
 // 展示的数组
+type City = {
+  citycode: string;
+  adcode: string;
+  name: string;
+  level: string,
+  center: string,
+  districts: City[]
+}
 const cityArray: Ref<
-  {
-    citycode: string;
-    adcode: string;
-    name: string;
-  }[]
+  City[]
 > = ref([]);
+
+// 清除CityText的值，等于关闭搜索
+const clearCityText = () => {
+  cityText.value = ''
+}
+// 选择了某个城市触发
+const ChooseCityWeather = (item: City) => {
+  // 请求对应编码的天气
+  getWeather(item.adcode).then((res) => {
+    console.log(res);
+  });
+// console.log(item);
+
+}
+
+
 </script>
 <template>
   <div class="view">
     <div class="view_let">左视图</div>
     <div class="view_center">
       <div class="view_center_search">
-        <input type="text" placeholder="搜索" v-model="cityText" />
-        <div class="view_center_search_view">
-          视图
-          <div
-            class="view_center_search_view_item"
-            v-for="item in cityArray"
-            :key="item.adcode"
-          >
-            {{ item }}
+        <div class="view_center_search_input">
+          <input type="text" placeholder="搜索" v-model="cityText" />
+          <span class="view_center_search_input_close" v-show="cityText !== ''" @click="clearCityText">
+            <el-icon>
+              <Close />
+            </el-icon>
+          </span>
+        </div>
+        <div class="view_center_search_view" v-if="cityText !== ''">
+          <div v-show="!isGetCitysFinally">{{ "加载中。。。" }}</div>
+          <div v-show="cityArray.length === 0 && isGetCitysFinally && !chineseReg.test(cityText)">{{ `搜索失败，请遵循以下规则
+            ：只支持单个关键词语搜索关键词支持:行政区名称、城市编码、邮件编码
+            例如，搜索省份（例如山东），能够显示市（例如济南），区（例如历下区）`
+            }}</div>
+          <div class="view_center_search_view_item" v-for="(item, index) in cityArray" :key="item.adcode"
+            v-if="isGetCitysFinally" @click="ChooseCityWeather(item)">
+
+            <div class="view_center_search_view_item_index"> {{ index }}</div>
+            <div class="view_center_search_view_item_name">{{ item.name }}</div>
+            <div class="view_center_search_view_item_adcode">{{ item.adcode }}</div>
+
           </div>
         </div>
       </div>
@@ -91,20 +157,73 @@ const cityArray: Ref<
   background-color: blue;
 }
 
-.view_center_search {
+.view_center_search {}
+
+.view_center_search_input {
+  position: relative;
+  display: flex;
 }
 
-.view_center_search input {
-  /* height: 4vw; */
-  min-height: 4vh;
+.view_center_search_input input {
+  min-height: 2.5vw;
   width: 100%;
   font-size: 1rem;
-  transition: all 1s;
+  border: 0px;
+  border-radius: 2rem;
+
+  transition: all .5s;
 }
 
-.view_center_search :hover {
-  height: 4vw;
-  min-height: 4vh;
-  transition: all 1s;
+.view_center_search_input_close {
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
+
+.view_center_search_view {
+  height: 40vh;
+  overflow: scroll;
+  background-color: aliceblue;
+}
+
+/* 隐藏滚动条 */
+.view_center_search_view::-webkit-scrollbar {
+  width: 0;
+}
+
+.view_center_search_view::-webkit-scrollbar {
+  height: 0;
+}
+
+.view_center_search_view_item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.view_center_search_view_item div {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4vh;
+  min-height: 20px;
+  border: .5px dotted #8299ff;
+
+}
+
+.view_center_search_view_item .view_center_search_view_item_index {
+  flex: .5;
+  color: #0021dd;
+}
+
+.view_center_search_view_item .view_center_search_view_item_name {
+  flex: 3;
+  font-weight: 900;
+
+}
+
+.view_center_search_view_item .view_center_search_view_item_adcode {}
 </style>
--- src/assets/ts/debounce.ts
+++ src/assets/ts/debounce.ts
@@ -1,41 +1,13 @@
-type Func = (...args: any[]) => any
-export function debounce(func: Func, delay: number, immediate?: boolean, resultCallback?: Func) {
-    // ReturnType 可以方便地获取函数的返回类型
-    let timer: null | ReturnType<typeof setTimeout> = null;
-    let isInvoke = false;
-    const _debounce = function(this: unknown, ...args: any[]) {
-      return new Promise((resolve, reject) => {
-        
-        if (timer) clearTimeout(timer);
-        if (immediate && !isInvoke) {
-          try {
-            const result = func.apply(this, args);
-            if (resultCallback) resultCallback(result);
-            resolve(result);
-          } catch (e) {
-            reject(e);
-          }
-          isInvoke = true;
-        } else {
-          timer = setTimeout(() => {
-            try {
-              const result = func.apply(this, args);
-              if (resultCallback) resultCallback(result);
-              resolve(result);
-            } catch (e) {
-              reject(e);
-            }
-            isInvoke = false;
-            timer = null;
-          }, delay);
-        }
-      });
-    };
-    _debounce.cancel = function() {
-      if (timer) clearTimeout(timer);
-      isInvoke = false;
-      timer = null;
-    };
-    return _debounce;
+let timer: any;
+export function debounce(fun: Function, delay: number) {
+  return function (this:any) {
+      var args = arguments;
+      if (timer) {
+          clearTimeout(timer);
+      }
+      
+      timer = setTimeout(() => {
+          fun.apply(this, args);
+      }, delay);
   }
-  
+}
