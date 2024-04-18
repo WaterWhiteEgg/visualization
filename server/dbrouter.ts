8fdb37f06bfbc513dd01fe9761e858fda65ee387
White Egg <2733259150@qq.com>
2024-04-18 10:10:39 +0800

    不要使用数据库先测试

--- server/dbrouter.ts
+++ server/dbrouter.ts
@@ -6,52 +6,52 @@ const connection = require("./dbmain")
 
 const dbrouter: Router = require("./router");
 
-dbrouter.post("/commit", async (req, res) => {
+// dbrouter.post("/commit", async (req, res) => {
 
-    const { name, password, resource, desc, region } = req.body
-    let set = `INSERT INTO _user (username, password, status,gender,descs) VALUES ('${name}', '${password}', '${resource}','${region}','${desc}')`;
-    connection.query(set, function (err: any, results: any, fields: any) {
-        // 插入成功生成token
-        let token: string;
-        if (resource === 1) {
-            token = jwt.sign(
-                { user: { name } },
-                SECRET_KEY,
-                { expiresIn: '720h' }
-            )
-        } else {
-            token = jwt.sign(
-                { user: { name } },
-                SECRET_KEY,
-                { expiresIn: '60s' })
-        }
+//     const { name, password, resource, desc, region } = req.body
+//     let set = `INSERT INTO _user (username, password, status,gender,descs) VALUES ('${name}', '${password}', '${resource}','${region}','${desc}')`;
+//     connection.query(set, function (err: any, results: any, fields: any) {
+//         // 插入成功生成token
+//         let token: string;
+//         if (resource === 1) {
+//             token = jwt.sign(
+//                 { user: { name } },
+//                 SECRET_KEY,
+//                 { expiresIn: '720h' }
+//             )
+//         } else {
+//             token = jwt.sign(
+//                 { user: { name } },
+//                 SECRET_KEY,
+//                 { expiresIn: '60s' })
+//         }
 
-        console.log(err && "没有错误");
-        res.send({
-            status: 0,
-            message: "插入成功",
-            token,
-        })
-        res.end(); // 结束响应
-    })
+//         console.log(err && "没有错误");
+//         res.send({
+//             status: 0,
+//             message: "插入成功",
+//             token,
+//         })
+//         res.end(); // 结束响应
+//     })
 
-});
+// });
 
-dbrouter.post("/login", async (req, res) => {
+// dbrouter.post("/login", async (req, res) => {
 
-    // console.log(req.body);
-    const { name, password, resource, desc, region } = req.body
-    let set = `INSERT INTO _user (username, password, status,gender,descs) VALUES ('${name}', '${password}', '${resource}','${region}','${desc}')`;
-    connection.query(set, function (err: any, results: any, fields: any) {
-        console.log(err);
-        res.send({
-            status: 0,
-            message: "插入成功",
-        })
-        res.end(); // 结束响应
-    })
+//     // console.log(req.body);
+//     const { name, password, resource, desc, region } = req.body
+//     let set = `INSERT INTO _user (username, password, status,gender,descs) VALUES ('${name}', '${password}', '${resource}','${region}','${desc}')`;
+//     connection.query(set, function (err: any, results: any, fields: any) {
+//         console.log(err);
+//         res.send({
+//             status: 0,
+//             message: "插入成功",
+//         })
+//         res.end(); // 结束响应
+//     })
 
-});
+// });
 module.exports = dbrouter
 
 
