162fdf4be5908d5a949a8315d714bdc3253d174f
White Egg <2733259150@qq.com>
2024-06-15 14:00:23 +0800

    error能够接入status数据了

--- server/middleware/error.ts
+++ server/middleware/error.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from "express";
 // 自定义错误中间件
 export const error = (req: Request, res: Response, next: NextFunction) => {
   // 错误信息显示组件
-  res.cc = function (err: Error | string, status = 1) {
+  res.cc = function (err: Error | string, status = 1,HTMLstatus = 500) {
     console.log(err instanceof Error ? err.message : err);
     res
-      .status(500)
+      .status(HTMLstatus)
       .json({ status, error: err instanceof Error ? err.message : err });
   };
   next();
