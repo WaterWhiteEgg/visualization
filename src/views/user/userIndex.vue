191e3c5d8ef932fbe27f1710ca5dc7fa3b0ea211
White Egg <2733259150@qq.com>
2024-07-12 12:50:14 +0800

    赋值数据

--- src/views/user/userIndex.vue
+++ src/views/user/userIndex.vue
@@ -5,7 +5,7 @@ import { useRegister } from "@/stores/register";
 import { getUserData, userVerifyToken, getEasyUserData } from "@/network/user";
 import { useRoute } from "vue-router";
 // 挂载route实例
-const route = useRoute()
+const route = useRoute();
 // 渲染的对象
 const userData = ref({});
 // 判断是否显示token本体才能执行的flag
@@ -17,19 +17,18 @@ onMounted(() => {
 
   // 由于在解析token时已经获取到数据了，但为了安全起见，进入这个界面需要再一次验证token
   // console.log(userinfo);
-// console.log(route.params.user_id);
+  // console.log(route.params.user_id);
 
   // 判断token存在再做配置
   if (!window.localStorage.getItem("token")) {
     // 3若token没有，则无论如何都进入基础面板信息
     isShowMainUserFlag.value = false;
     // 请求基本数据,使用动态路由提供的值去寻找
-    // console.log(route.params.user_id as string);
-    
-    getEasyUserData(route.params.user_id as string).then((res)=>{
-      console.log(res);
-      
-    })
+
+    getEasyUserData(route.params.user_id as string).then((res) => {
+      console.log(JSON.parse(res.data.data));
+      userData.value = JSON.parse(res.data.data);
+    });
   }
   // 有token的情况
   else {
@@ -52,11 +51,7 @@ onMounted(() => {
     // 重新显示mainviewIndex
     // usePopup().changeisOpenMainviewIndex(true);
   });
-const props = withDefaults(
-  defineProps<{
-  }>(),
-  {}
-);
+const props = withDefaults(defineProps<{}>(), {});
 const emits = defineEmits<{
   (e: "emit", i: void): void;
 }>();
