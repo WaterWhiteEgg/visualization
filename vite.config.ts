6606243d0c52cccf88c9142cedd1a6fd9f5003f5
White Egg <2733259150@qq.com>
2024-09-07 14:51:23 +0800

    cdn代理echarts，同时添加了包监测插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,48 @@ import ConditionalCompile from "vite-plugin-conditional-compiler";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import viteCompression from "vite-plugin-compression";
-import { Options, Plugin as importToCDN } from "vite-plugin-cdn-import";
+import { visualizer } from "rollup-plugin-visualizer";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import cdn from "vite-plugin-cdn-import";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
+    // 打包体积分析
+    visualizer({
+      open: true,
+      filename: "visualizer.html", //分析图生成的文件名
+    }),
     // 实现条件编译
     ConditionalCompile(),
     // 压缩代码
-    viteCompression(),
+    viteCompression({
+      verbose: true, // 是否在控制台中输出压缩结果
+      threshold: 1024, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+    }),
+    // 按需导入element plus组件。实现自动按需引入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // 按需导入element plus 自定义主题。实现组件自动按需导入
+    Components({
+      resolvers: [ElementPlusResolver({ importStyle: "css" })],
+    }),
+
     // cdn服务
-    importToCDN({})
-    
+    cdn({
+      modules: [
+        {
+          name: "echarts",
+          var: "echarts",
+          path: ["https://cdn.bootcdn.net/ajax/libs/echarts/5.5.0/echarts.js"],
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
